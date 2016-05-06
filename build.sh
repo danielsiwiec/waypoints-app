81538fe084fc656d0060f0672b14507bba90a5b6
Dan Siwiec <daniel.siwiec@gmail.com>
2016-05-05 23:26:50 -0700

    Add build script. No more eclipsing

---
+++ build.sh
@@ -0,0 +1,4 @@
+monkeyc --output output.prg \
+-m manifest.xml \
+-z resources/strings/strings.xml:resources/drawables/drawables.xml \
+source/WaypointsApp.mc source/HashView.mc source/DigitFactory.mc
