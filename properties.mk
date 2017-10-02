9282998d09bc5b158ebbb6af52786bc5dc49e11c
Dan Siwiec <daniel.siwiec@gmail.com>
2017-10-02 12:48:57 -0700

    update sdk version

--- properties.mk
+++ properties.mk
@@ -1,5 +1,5 @@
 DEVICE = fenix5s
-SDK_HOME = /Users/dsiwiec/connectiq-sdk-mac-2.2.4
+SDK_HOME = /Users/dsiwiec/connectiq-sdk-mac-2.3.4
 DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
 SUPPORTED_DEVICES_LIST = fenix3 fenix3_hr fenix5 fenix5s fenix5x epix
 PRIVATE_KEY = /Users/dsiwiec/.ssh/id_rsa_garmin.der
