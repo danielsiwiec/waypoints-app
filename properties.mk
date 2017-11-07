570cd641b23ed38a88db89cf41c6a68d2d90f66a
Dan Siwiec <daniel.siwiec@gmail.com>
2017-11-07 09:26:00 -0800

    Add fr935 as a supported device

--- properties.mk
+++ properties.mk
@@ -1,5 +1,5 @@
 DEVICE = fenix5s
 SDK_HOME = /Users/dsiwiec/connectiq-sdk-mac-2.3.4
 DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
-SUPPORTED_DEVICES_LIST = fenix3 fenix3_hr fenix5 fenix5s fenix5x epix
+SUPPORTED_DEVICES_LIST = fenix3 fenix3_hr fenix5 fenix5s fenix5x epix fr935
 PRIVATE_KEY = /Users/dsiwiec/.ssh/id_rsa_garmin.der
