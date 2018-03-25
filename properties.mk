25916c3bfa5aacfb32d7a194846200308eaf163b
Dan Siwiec <daniel.siwiec@gmail.com>
2018-03-24 17:56:53 -0700

    Update to use jungle file

--- properties.mk
+++ properties.mk
@@ -1,5 +1,5 @@
 DEVICE = fenix5s
-SDK_HOME = /Users/dsiwiec/connectiq-sdk-mac-2.3.4
+SDK_HOME = /Users/dsiwiec/connectiq-sdk-mac-2.4.4
 DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
-SUPPORTED_DEVICES_LIST = fenix3 fenix3_hr fenix5 fenix5s fenix5x epix fr935
+SUPPORTED_DEVICES_LIST = d2bravo d2bravo_titanium d2charlie descentmk1 epix fr645 fr645m fr935 fenix3 fenix3_hr fenix5 fenix5s fenix5x fenixchronos
 PRIVATE_KEY = /Users/dsiwiec/.ssh/id_rsa_garmin.der
