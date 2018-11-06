43a552079adef7e7ddf2a550bf01c8947b5fac86
Dan Siwiec <daniel.siwiec@gmail.com>
2018-11-05 21:36:05 -0800

    Read devices from manifest

--- properties.mk
+++ properties.mk
@@ -1,5 +1,4 @@
 DEVICE = fenix5s
 SDK_HOME = /Users/daniel.siwiec/connectiq-sdk-mac-3.0.4
 DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
-SUPPORTED_DEVICES_LIST = d2bravo d2bravo_titanium d2charlie descentmk1 epix fr645 fr645m fr935 fenix3 fenix3_hr fenix5 fenix5s fenix5x fenixchronos
 PRIVATE_KEY = /Users/daniel.siwiec/.ssh/id_rsa_garmin.der
