cfa991ce30208b5b7088770cab15145b76562beb
Dan Siwiec <daniel.siwiec@gmail.com>
2018-11-05 21:18:55 -0800

    Update props

--- properties.mk
+++ properties.mk
@@ -1,5 +1,5 @@
 DEVICE = fenix5s
-SDK_HOME = /Users/dsiwiec/connectiq-sdk-mac-2.4.4
+SDK_HOME = /Users/daniel.siwiec/connectiq-sdk-mac-3.0.4
 DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
 SUPPORTED_DEVICES_LIST = d2bravo d2bravo_titanium d2charlie descentmk1 epix fr645 fr645m fr935 fenix3 fenix3_hr fenix5 fenix5s fenix5x fenixchronos
-PRIVATE_KEY = /Users/dsiwiec/.ssh/id_rsa_garmin.der
+PRIVATE_KEY = /Users/daniel.siwiec/.ssh/id_rsa_garmin.der
