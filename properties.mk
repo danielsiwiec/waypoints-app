13e1bc176073bb32ea24fce5dd00f61962879345
Dan Siwiec <daniel.siwiec@gmail.com>
2020-05-23 17:57:16 -0700

    Add support for edge530

--- properties.mk
+++ properties.mk
@@ -1,4 +1,4 @@
-DEVICE = fr935
+DEVICE = edge530
 SDK_HOME = /Users/daniel.siwiec/tools/connectiq-sdk-mac-3.1.8
 DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
 PRIVATE_KEY = /Users/daniel.siwiec/.ssh/id_rsa_garmin.der
