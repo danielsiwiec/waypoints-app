94ab2c38d90b94a1faf2e7e4218ebf3cb67cb208
Dan Siwiec <daniel.siwiec@gmail.com>
2020-05-23 17:51:25 -0700

    Add sdkmanrc file

--- properties.mk
+++ properties.mk
@@ -1,4 +1,4 @@
 DEVICE = fr935
-SDK_HOME = /Users/daniel.siwiec/tools/connectiq-sdk-mac-3.1.3
+SDK_HOME = /Users/daniel.siwiec/tools/connectiq-sdk-mac-3.1.8
 DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
 PRIVATE_KEY = /Users/daniel.siwiec/.ssh/id_rsa_garmin.der
