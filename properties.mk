477b5b98c9891b1d502fd8c9f8821b900d1e53f7
Dan Siwiec <daniel.siwiec@gmail.com>
2022-02-16 15:10:44 -0800

    Add devices

--- properties.mk
+++ properties.mk
@@ -1,4 +1,4 @@
-DEVICE = edge1030bontrager
+DEVICE = descentmk2s
 SDK_HOME = /Users/dan.siwiec/Library/Application\ Support/Garmin/ConnectIQ/Sdks/connectiq-sdk-mac-4.0.8-2022-01-20-c823f71a9/
 DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
 PRIVATE_KEY = /Users/dan.siwiec/.ssh/garmin_developer_key.der
